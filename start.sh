c158a9a4c3bcf06e39c488170e1c0c37345d31f0
Evan <zhidong.s@outlook.com>
2022-12-27 18:10:13 +0800

    commit

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #!/usr/bin bash
 
-#python3 smart_proxy_pool.py server
+python3 smart_proxy_pool.py server &
 python3 smart_proxy_pool.py schedule
-#python3 smart_proxy_pool.py server &
-#python3 smart_proxy_pool.py schedule
