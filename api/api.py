13626bfe62c3009b15f59b8f270dfe32e7194dc7
evan <zhidong.s@outlook.com>
2023-01-02 15:11:49 +0800

    feat: 统一返回格式

--- api/api.py
+++ api/api.py
@@ -5,13 +5,14 @@ __author__ = 'Evan'
 
 import sys
 from os.path import dirname, abspath
+from typing import Union
 
 base_path = dirname(dirname(abspath(__file__)))
 sys.path.insert(0, base_path)
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, status
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import JSONResponse, FileResponse
+from fastapi.responses import JSONResponse, FileResponse, Response
 import uvicorn
 from helper.proxy import Proxy
 from handler.proxy_handler import ProxyHandler
@@ -20,7 +21,7 @@ from handler.config_handler import ConfigHandler
 app = FastAPI()
 app.mount('/static', StaticFiles(directory='static'), name='static')
 templates = Jinja2Templates(directory='templates')
-# router = app.router('/api/v1')
+
 
 conf = ConfigHandler()
 proxy_handler = ProxyHandler()
@@ -43,21 +44,30 @@ async def favicon():
 
 @app.get('/')
 async def index():
-    return {'url': api_list}
+    data = api_list
+    return success(data=data)
 
 
 @app.get('/get')
 async def get(request: Request):
     https = request.method.lower() == 'https'
     proxy = proxy_handler.get(https)
-    return proxy.to_dict if proxy else {"code": 0, "src": "no proxy"}
+    if proxy:
+        data = proxy.to_dict
+        return success(data=data)
+    else:
+        return error(message='NO PROXY')
 
 
 @app.get('/pop')
 async def pop(request: Request):
     https = request.method.lower() == 'https'
     proxy = proxy_handler.pop(https)
-    return proxy.to_dict if proxy else {"code": 0, "src": "no proxy"}
+    if proxy:
+        data = proxy.to_dict
+        return success(data=data)
+    else:
+        return error(message='NO PROXY')
 
 
 @app.get('/refresh')
@@ -70,14 +80,16 @@ async def refresh():
 async def get_all(request: Request):
     https = request.method.lower() == 'https'
     proxies = proxy_handler.get_all(https)
-    return JSONResponse([_.to_dict for _ in proxies])
+    data = [_.to_dict for _ in proxies]
+    return success(data=data)
 
 
 @app.get('/delete')
 async def delete(request: Request):
     proxy = request.query_params.get('proxy')
-    status = proxy_handler.delete(Proxy(proxy))
-    return {"code": 0, "src": status}
+    _status = proxy_handler.delete(Proxy(proxy))
+    data = _status
+    return success(data=data)
 
 
 @app.get('/count')
@@ -90,7 +102,8 @@ async def count():
         http_type_dict[http_type] = http_type_dict.get(http_type, 0) + 1
         for source in proxy.source.split('/'):
             source_dict[source] = source_dict.get(source, 0) + 1
-    return {"http_type": http_type_dict, "source": source_dict, "count": len(proxies)}
+    data = {"http_type": http_type_dict, "source": source_dict, "count": len(proxies)}
+    return success(data=data)
 
 
 @app.get('/agents')
@@ -103,6 +116,28 @@ async def agents(request: Request):
 
 def run_fastapi():
     uvicorn.run('api.api:app', host=conf.server_host, port=conf.server_port, reload=True)
+
+
+def success(*, data: Union[list, dict, str]) -> Response:
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={
+            'code': 200,
+            'message': 'Success',
+            'data': data,
+        }
+    )
+
+
+def error(*, data: str = None, message: str = 'BAD REQUEST') -> Response:
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content={
+            'code': 400,
+            'message': message,
+            'date': data,
+        }
+    )
 
 
 if __name__ == '__main__':
